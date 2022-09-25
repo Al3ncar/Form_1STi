c61c8b5512c4d117d3302c4a3c71fb151e242fec
Al3ncar <alencar29htcss@gmail.com>
2022-09-25 12:38:49 -0300

    Form_1STi

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import{Routes,Route}from "react-router-dom"
-import Main from "../main/main"
+import Main from "../main/mains/F_index"
 import Sing from "../../Pages/SingIn/index"
 import Cong from "../../Pages/Conclud/index"
 
 export default function index() {
   return (
     <Routes>
-        <Route path="/" element={<Main/>}/>
+        <Route path="" element={<Main/>}/>
         <Route path="/sing" element={<Sing/>}/>
         <Route path="/cong" element={<Cong/>}/>
     </Routes>
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,5 +2,5 @@ import React from "react";
 import FIndex from "./mains/F_index"
 
 export default function Main(){
-    return(<> <FIndex/> </>)
+    return<FIndex/>
 }
